af0ab3f1512ec952c4159109eb1c0bee4c4d2159
tomklecha <tom.klecha.dev@gmail.com>
2023-10-30 23:20:04 +0100

    added necessary to start libs. With new toml approach

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    // trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.sqlDelight).apply(false)
+    alias(libs.plugins.hilt).apply(false)
 }
