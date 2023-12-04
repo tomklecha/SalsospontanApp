910593423e7fd8e67bc55173da92edd066a486b4
tomklecha <tom.klecha.dev@gmail.com>
2023-12-04 22:09:57 +0100

    created basic activity screen with navigation to separate composable screens

--- androidApp/src/main/java/com/tkdev/salsospontanapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/tkdev/salsospontanapp/android/MainActivity.kt
@@ -4,43 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import com.tkdev.salsospontanapp.ui.artists.AndroidArtistViewModel
-import com.tkdev.salsospontanapp.ui.artists.ArtistEvent
-import org.koin.compose.koinInject
+import com.tkdev.salsospontanapp.android.compose.MainActivityScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Tested building of artists viewModel
-                    val viewModel = koinInject<AndroidArtistViewModel>()
-                    val state = viewModel.state.collectAsState()
-                    GreetingView(viewModel::onEvent, state.value.artistList.size.toString())
+                    MainActivityScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun GreetingView(onEvent: (ArtistEvent) -> Unit, text: String) {
-    Button(
-        onClick = { onEvent(ArtistEvent.AddArtist) },
-        content =
-        {
-            Text(text = text)
-        }
-    )
 }
---
+++ androidApp/src/main/java/com/tkdev/salsospontanapp/ui/venues/compose/VenuesScreen.kt
@@ -0,0 +1,18 @@
+package com.tkdev.salsospontanapp.ui.venues.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun VenuesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(
+            color = Color.Green
+        )
+    ) {
+    }
+}
